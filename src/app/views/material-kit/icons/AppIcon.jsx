e5278601d4982e0171d6434ed346d380bd38b5ea
priyank-el <priyank.a@elaunchinfotech.in>
2023-11-21 12:41:30 +0530

    fix:update product side and implement search functionality

--- src/app/views/material-kit/icons/AppIcon.jsx
+++ src/app/views/material-kit/icons/AppIcon.jsx
@@ -1,7 +1,10 @@
-import { Box, Stack, styled } from "@mui/material";
+import { Box, Stack, styled, TextField } from "@mui/material";
 import { SimpleCard } from "app/components";
 import ProductAddForm from "../forms/ProductAddForm";
 import ProductListForm from "../forms/ProductListForm";
+import { Span } from "app/components/Typography";
+import { fetchAllProducts } from "store/actions/productActions";
+import { useDispatch } from 'react-redux';
 
 const Container = styled("div")(({ theme }) => ({
   margin: "30px",
@@ -14,6 +17,9 @@ const Container = styled("div")(({ theme }) => ({
 
 
 const ProductAdd = () => {
+
+  const dispatch = useDispatch()
+
   return (
     <Container>
       <Stack spacing={3}>
@@ -25,6 +31,14 @@ const ProductAdd = () => {
         {/* </SimpleCard> */}
 
         <SimpleCard title="All Products">
+          <Box display={'flex'} justifyContent={'end'}>
+            <Span className='mt-3 px-3'>Search</Span>
+            <TextField onChange={(e) => {
+              e.preventDefault()
+              dispatch(fetchAllProducts(e.target.value))
+            }
+            } />
+          </Box>
           <ProductListForm />
         </SimpleCard>
         {/* </SimpleCard> */}
--- src/store/actions/productActions.js
+++ src/store/actions/productActions.js
@@ -6,11 +6,13 @@ import { toast } from "react-toastify"
 
 const token = localStorage.getItem('JwtToken')
 
-export const fetchAllProducts = () => {
+export const fetchAllProducts = (value) => {
     return async (dispatch) => {
         dispatch(isLoading(true))
         try {
-            const { data } = await axios.get('http://localhost:3003/admin/all-products', { headers: { "env": "test", "Authorization": token } })
+            if (!value) value = ''
+            value = value.trim()
+            const { data } = await axios.get(`http://localhost:3003/admin/all-products?value=${value}`, { headers: { "env": "test", "Authorization": token } })
             if (data) {
                 dispatch({ type: productActionType.FETCH_ALL_PRODUCTS, payload: data })
                 dispatch(isLoading(false))
@@ -39,6 +41,32 @@ export const createProduct = (productData, productImage) => {
 
             if (data) {
                 dispatch({ type: productActionType.CREATE_PRODUCT, payload: data })
+                toast.success(data.message)
+                dispatch(fetchAllProducts())
+            }
+        } catch (error) {
+            console.log("Error is -> ", error);
+        }
+    }
+}
+export const updateProduct = (id, productData, productImage) => {
+    const { product_name, product_description, category_Id, brand_Id } = productData
+    return async (dispatch) => {
+        try {
+            debugger
+            const { data } = await axios.put(`http://localhost:3003/admin/update-product?id=${id}`,
+                {
+                    productName: product_name,
+                    productDescription: product_description,
+                    productCategory: category_Id,
+                    productBrand: brand_Id,
+                    image: productImage
+                },
+                { headers: { "env": "test", "Authorization": token } }
+            )
+
+            if (data) {
+                dispatch({ type: productActionType.UPDATE_PRODUCT, payload: data })
                 toast.success(data.message)
                 dispatch(fetchAllProducts())
             }
